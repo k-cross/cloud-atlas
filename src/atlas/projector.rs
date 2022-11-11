9d0e77f67728493be6315429411d1d927fcf3530
KMC <ken.q.cross@gmail.com>
2022-11-10 23:43:20 -0800

    added projector

---
+++ src/atlas/projector.rs
@@ -0,0 +1,12 @@
+// dependencies
+use graph_builder;
+use aws_sdk_config::model::ResourceIdentifier;
+
+// system
+use std::collections::HashMap
+
+fn main() {}
+
+fn project(data: HashMap<String, ResourceIdentifier>) {
+  
+}
