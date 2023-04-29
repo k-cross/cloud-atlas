e8229963697b806cb21f1d1d3bc47ea29b1912f2
KMC <ken.q.cross@gmail.com>
2023-04-28 19:57:09 -0700

    add the collector to the provider

--- src/cloud/amazon/provider.rs
+++ src/cloud/amazon/provider.rs
@@ -1,11 +1,15 @@
-use crate::cloud::amazon::{instance, resource};
+use crate::cloud::amazon::{instance, network, resource};
 use crate::cloud::definition::Provider;
 
 pub async fn build_aws(verbose: bool, region: String) -> Result<Provider, Box<dyn std::error::Error>> {
     let c = resource::collector::runner(verbose, region.as_str());
     let i = instance::collector::runner(region.as_str());
+    let n = network::collector::runner(region.as_str());
+
+    // await for the collectors to finish
+    let nets = n.await?;
     let configs = c.await?;
     let (running_insts, _offline_insts) = i.await?;
 
-    Ok(Provider::AWS(vec![running_insts, configs]))
+    Ok(Provider::AWS(vec![running_insts, configs, nets]))
 }
