a0c89d89526185526a4ca01506ff4742dd6e2193
KMC <kencross@dropbox.com>
2023-05-08 11:14:37 -0700

    fix missing iam provisioner // add build script

--- src/cloud/amazon/provider.rs
+++ src/cloud/amazon/provider.rs
@@ -3,6 +3,7 @@ use crate::cloud::amazon::{
     instance, 
     network,
     resource,
+    iam,
     lambda,
     eventbridge,
 };
@@ -15,14 +16,16 @@ pub async fn build_aws(verbose: bool, region: String) -> Result<Provider, Box<dy
     let container_col = container_service::collector::runner(region.as_str());
     let lambda_col    = lambda::collector::runner(region.as_str());
     let evts_col      = eventbridge::collector::runner(region.as_str());
+    let iam_col       = iam::collector::runner(region.as_str());
 
     // await for the collectors to finish
-    let nets = network_col.await?;
-    let resources = resource_col.await?;
-    let running_insts = instance_col.await?;
-    let conts = container_col.await?;
-    let funcs = lambda_col.await?;
-    let evts = evts_col.await?;
+    let  nets           =  network_col.await?;
+    let  resources      =  resource_col.await?;
+    let  running_insts  =  instance_col.await?;
+    let  conts          =  container_col.await?;
+    let  funcs          =  lambda_col.await?;
+    let  evts           =  evts_col.await?;
+    let  iam            =  iam_col.await?;
 
-    Ok(Provider::AWS(vec![running_insts, resources, nets, conts, funcs, evts]))
+    Ok(Provider::AWS(vec![running_insts, resources, nets, conts, funcs, evts, iam]))
 }
